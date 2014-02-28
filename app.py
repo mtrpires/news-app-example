60000cc445d250b6b5c4160abce020c5aa16fd2a
mtrpires <mtrpires@outlook.com>
2014-02-28 11:42:07 -0500

    made a map on the index page

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def index():
 @app.route('/<number>/')
 def detail(number):
     return render_template('detail.html',
-    object = csv_dict[number],
+        object = csv_dict[number],
     )
 
 if __name__ == '__main__':
